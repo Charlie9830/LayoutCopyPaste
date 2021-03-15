5b03f018df12b747c039789e343e6cb395848a3f
Charlie9830 <charliewjhall@gmail.com>
2021-03-15 18:57:58 +1100

    Added gitignore

--- main.lua
+++ main.lua
@@ -3,9 +3,8 @@ local LayoutIO = require("LayoutIO")
 local Mutation = require("Mutation")
 local inspect = require("inspect")
 
-local data = LayoutIO.read("helloworld.xml")
+local data = LayoutIO.read("C:\\ProgramData\\MA Lighting Technologies\\grandma\\gma2_V_3.9.60\\temp\\helloworld.xml")
 
---print(data.MA.Info._attr.datetime)
+-- print(data.MA.Info._attr.datetime)
 local selectionRec = Mutation.findSelectionRec(data)
 
-print(inspect(selectionRec))
